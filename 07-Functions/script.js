f53b587f4a1e6e9fb8934f45c6a711b044f0506d
ardevi44 <arbey9621@gmail.com>
2024-07-29 02:38:58 -0600

    High order functions and callbacks

--- 07-Functions/script.js
+++ 07-Functions/script.js
@@ -54,3 +54,30 @@ const newPassport = function (person) {
 // Two functions manipulating the same object
 newPassport(jonas);
 checkIn(flight, jonas);
+
+// High order functions and callbacks
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformerd by ${fn.name}`);
+};
+
+transformer("Javascript is the best!", upperFirstWord);
+transformer("Javascript is the best!", oneWord);
+
+const high5 = function (name) {
+  console.log(`👋 Hi ${name}`);
+};
+
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
