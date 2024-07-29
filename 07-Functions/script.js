935c2a6597d98881e12829f8cb9a02ec393082af
ardevi44 <arbey9621@gmail.com>
2024-07-29 16:27:40 -0600

    The call method in js

--- 07-Functions/script.js
+++ 07-Functions/script.js
@@ -98,3 +98,45 @@ const greet2 = greeting => name => console.log(`${greeting} ${name}`);
 
 greet("Hey")("Marisol");
 greet2("Hi")("Arbey");
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, "Jonas Schmedtman");
+lufthansa.book(635, "John Smith");
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+// * The call method
+
+const book = lufthansa.book;
+// book(23, "Sarah Williams");
+
+book.call(eurowings, 23, "Sarah Williams");
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Mary Cooper");
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 583, "Mary Cooper");
+console.log(swiss);
