4a8091c17fb7497b7c8cbea5ebafa1c420d278f2
ardevi44 <arbey9621@gmail.com>
2024-07-29 00:42:21 -0600

    Passing by value and reference

--- 07-Functions/script.js
+++ 07-Functions/script.js
@@ -20,3 +20,37 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 createBooking("LH123");
 createBooking("LH123", 2, 455);
 createBooking("LH123", undefined, 455); // The same to skip the value it will be 1
+
+// Passing arguments value vs reference
+
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 23456789284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999"; // This is a value pass
+  passenger.name = "Mr. " + passenger.name; // This is a reference pass
+
+  if (passenger.passport === 23456789284) {
+    console.log("Checked in");
+  } else {
+    console.warn("Wrong passport");
+  }
+};
+
+checkIn(flight, jonas);
+
+console.log(flight); // This value doesn't change ...
+console.log(jonas); // But this one does.
+
+// Changing a passport
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+// Two functions manipulating the same object
+newPassport(jonas);
+checkIn(flight, jonas);
