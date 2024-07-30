8c50cd9aaf40966b93e1cbff94ab22fd8f9197c6
ardevi44 <arbey9621@gmail.com>
2024-07-30 16:33:52 -0600

    Create a file for arrays examples

---
+++ 08-Working-Arrays/arrays.js
@@ -0,0 +1,104 @@
+// DOC: Array methods
+
+let arr = ["a", "b", "c", "d", "e"];
+
+console.log(arr);
+console.log(arr.slice(2)); // Start index
+console.log(arr.slice(2, 5)); // Start index and final index excluding it
+console.log(arr.slice(-2)); // Two positions from the end
+console.log(arr.slice(-1)); // Just the las index
+console.log(arr.slice(1, -2)); // The second parameter starts from the end until the second element from the end but excluding it so this is b and c elements.
+console.log(arr.slice());
+console.log([...arr]);
+
+// DOC: The splice method
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb");
+// Inserts at index 1
+console.log(months);
+// Expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, "May");
+// Replaces 1 element at index 4
+console.log(months);
+// Expected output: Array ["Jan", "Feb", "March", "April", "May"]
+
+// * The Reverse method
+const arr2 = ["j", "i", "h", "g", "f"];
+console.log("New arr2: ", arr2);
+console.log("arr2 reversed: ", arr2.reverse());
+
+// * The Concat method
+const letters = arr.concat(arr2);
+console.log("letters array concat: ", letters);
+
+// * Join method
+console.log(letters.join(" - "));
+
+// At Method
+console.log(arr2[0]);
+console.log(arr2.at(0));
+
+// Getting last array element
+console.log(arr2[arr2.length - 1]);
+console.log(arr2.slice(-1)[0]);
+// New method
+console.log(arr2.at(-1));
+console.log("Ardevi44".at(0));
+console.log("Ardevi44".at(-1));
+
+// DOC: ForEach Array method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+/*
+NOTE: Parameters order
+1. The current element
+2. The index
+3. The array
+*/
+
+movements.forEach(function (movement, index, array) {
+  console.log(
+    movement > 0
+      ? `[${index + 1}] -> ${movement} [deposited]`
+      : `[${index + 1}] -> ${Math.abs(movement)} [withdrew]`
+  );
+});
+
+// DOC: ForEach method inside Maps and Set in JS
+
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+/*
+NOTE: Parameters order
+1. The value
+2. The key
+3. The map
+*/
+
+// Inside a Map
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Inside a Set
+// A set doesn't have keys or indexes, just values and these are uniques.
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
