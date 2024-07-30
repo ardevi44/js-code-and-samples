488fac9b0014825e81df2b442abd09796d038f1b
ardevi44 <arbey9621@gmail.com>
2024-07-30 03:34:38 -0600

    Creating and inserting dom elements

--- 08-Working-Arrays/script.js
+++ 08-Working-Arrays/script.js
@@ -61,6 +61,26 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  console.log(movements);
+  movements.forEach(function (mov, i) {
+    const typeOfMovement = mov > 0 ? "deposit" : "withdrawal";
+    const movementRow = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${typeOfMovement}">
+          ${i + 1} ${typeOfMovement}
+        </div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+    // console.log(movementRow);
+    containerMovements.insertAdjacentHTML("afterbegin", movementRow);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
