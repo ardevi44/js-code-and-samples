3cb57ff0d81246fb552fb8bb19f64cc07e37053f
ardevi44 <arbey9621@gmail.com>
2024-10-15 01:40:54 -0600

    Object create and challenge 2

--- 11-OOP/ObjectCreate.js
+++ 11-OOP/ObjectCreate.js
@@ -1,15 +1,28 @@
 "use strict";
 
+// This will be the prototype
 const PersonProto = {
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
   calcAge() {
     console.log(2037 - this.birthYear);
   },
 };
 
-const steven = Object.create(PersonProto);
-console.log(steven);
-steven.name = "Steven";
-steven.birthYear = 2002;
-steven.calcAge();
+const ardevi = Object.create(PersonProto);
 
-console.log(steven.__proto__ === PersonProto); // true
+// At this moment this is an empty object
+console.log(ardevi);
+console.log(ardevi.__proto__ === PersonProto);
+
+ardevi.init("ardevi44", 1990);
+
+console.log(ardevi);
+console.log(ardevi.firstName);
+console.log(ardevi.birthYear);
+
+// ardevi.name = "ardevi44";
+// ardevi.birthYear = 2002;
+// ardevi.calcAge();
