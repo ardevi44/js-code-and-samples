c6b249d44291dfc12bec89032a30e836721ddef9
ardevi44 <arbey9621@gmail.com>
2024-10-27 00:50:30 -0600

    A way to protect fields in ES6 classes

--- 11-OOP/more-class-examples.js
+++ 11-OOP/more-class-examples.js
@@ -1,28 +1,36 @@
+"use strict";
+
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // This is how we protect things in js between developers
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  get movements() {
+    return this._movements;
+  }
+
   // These two methods are our public interface for this object.
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan() {
+  // This method is not part of the public API of this class.
+  _approveLoan() {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -35,6 +43,13 @@ acc1.withdraw(140);
 acc1.requestLoan(1000);
 // We should never be allowed to access this method, cause is internal and responsibility of the requestLoan method
 acc1.approveLoan(1000);
-console.log(acc1);
 // We can access to this property, but it should be hide.
-console.log(acc1.pin);
+// console.log(acc1.pin);
+
+console.log(acc1);
+
+// We're still able to change this data, although the underscore
+// It's a convention put that for any other dev doesn't change that property directly
+// acc1._movements.push(34);
+
+console.log(acc1.movements);
