4319ca7330a593ea4a5871cc15458137a14de645
ardevi44 <arbey9621@gmail.com>
2026-07-27 00:55:23 -0600

    working with the while loop

---
+++ 02-Fundamentals-Part-2/while-loop.js
@@ -0,0 +1,15 @@
+let rep = 1;
+while (rep <= 10) {
+  console.log("Rep. " + rep);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) {
+    console.log("Loop is about to end...");
+  }
+}
